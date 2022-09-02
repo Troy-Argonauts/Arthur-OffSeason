25675ae93546749b0a1aaa54d5c3b1fdf3da1060
DarkMG1 <driverstation8728@gmail.com>
2022-09-01 20:44:25 -0400

    PID Tuning V2
    Co-Authored-By: Akash Sarada <95705632+akashsarada@users.noreply.github.com>
    Co-Authored-By: rohangajulaghosh <40278130+SolidityContract@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,7 +17,7 @@ public final class Constants {
         double WHEEL_REVOLUTION_DISTANCE_INCHES = WHEEL_DIAMETER_INCHES * Math.PI;
         double DISTANCE_CONVERSION = ENCODER_NU_PER_WHEEL_REVOLUTION / WHEEL_REVOLUTION_DISTANCE_INCHES;
         double NU_PER_INCH = ENCODER_NU_PER_WHEEL_REVOLUTION / WHEEL_REVOLUTION_DISTANCE_INCHES; //TODO: DELETE
-        double kP = 0.0058;
+        double kP = 0.0056;
         double kI = 0.032;
         double kD = 0.0036;
         double kP_TURN = 0.1;
